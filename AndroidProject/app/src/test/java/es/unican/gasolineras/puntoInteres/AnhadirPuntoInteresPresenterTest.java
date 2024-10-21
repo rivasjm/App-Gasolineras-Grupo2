40be6cce74e94778a69253a654371c378b053ae0
mda544 <miguel.delgadoa@alumnos.unican.es>
2024-10-21 18:21:47 +0200

    Clase de pruebas de anhadir un punto de interes implementada segun lo especificado en el plan de pruebas a fecha esrte commit

--- AndroidProject/app/src/test/java/es/unican/gasolineras/puntoInteres/AnhadirPuntoInteresPresenterTest.java
+++ AndroidProject/app/src/test/java/es/unican/gasolineras/puntoInteres/AnhadirPuntoInteresPresenterTest.java
@@ -1,30 +1,44 @@
 package es.unican.gasolineras.puntoInteres;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteConstraintException;
+import android.database.sqlite.SQLiteException;
 
 import androidx.test.core.app.ApplicationProvider;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.robolectric.RobolectricTestRunner;
 
 import es.unican.gasolineras.activities.puntoInteres.AnhadirPuntoInteresPresenter;
 import es.unican.gasolineras.activities.puntoInteres.IAnhadirPuntoInteresView;
 import es.unican.gasolineras.model.GasolinerasResponse;
 import es.unican.gasolineras.model.PuntoInteres;
+import es.unican.gasolineras.repository.AppDatabase;
+import es.unican.gasolineras.repository.DbFunctions;
 import es.unican.gasolineras.repository.IPuntosInteresDao;
 
 @RunWith(RobolectricTestRunner.class)
 public class AnhadirPuntoInteresPresenterTest {
 
+    AppDatabase db;
+
+    Context context = ApplicationProvider.getApplicationContext();
+
     private IAnhadirPuntoInteresView vistaMock;
-    private IPuntosInteresDao puntosInteresDaoMock;
+    private IPuntosInteresDao puntosInteresDao;
     private AnhadirPuntoInteresPresenter presenter;
 
     String nombreStr = "casa";
@@ -34,21 +48,20 @@ public class AnhadirPuntoInteresPresenterTest {
     double latitud = Double.parseDouble(latitudStr);
     double longitud = Double.parseDouble(longitudStr);
 
-    PuntoInteres p1 = new PuntoInteres();
+    PuntoInteres p;
 
     @Before
     public void setUp() {
 
         vistaMock = mock(IAnhadirPuntoInteresView.class);
-        puntosInteresDaoMock = mock(IPuntosInteresDao.class);
 
-        p1.setNombre(nombreStr);
-        p1.setLatitud(latitud);
-        p1.setLongitud(longitud);
+        db = DbFunctions.generaBaseDatosPuntosInteres(context);
+        puntosInteresDao = db.puntosInteresDao();
+        //puntosInteresDao.delete(any(PuntoInteres.class));
 
-        when(puntosInteresDaoMock.loadByName(nombreStr)).thenReturn(p1);
+        when(vistaMock.getPuntosInteresDAO()).thenReturn(puntosInteresDao);
 
-        when(vistaMock.getPuntosInteresDAO()).thenReturn(puntosInteresDaoMock);
+        p = new PuntoInteres(nombreStr, latitud, longitud);
 
         presenter = new AnhadirPuntoInteresPresenter(vistaMock);
     }
@@ -58,14 +71,19 @@ public class AnhadirPuntoInteresPresenterTest {
 
         // Caso valido
         presenter.onGuardarPuntoInteresClicked(nombreStr, latitudStr, longitudStr);
-        verify(puntosInteresDaoMock).insertAll(p1);
+        PuntoInteres puntoCapturado = puntosInteresDao.loadByName(nombreStr);
+        assertEquals(nombreStr, puntoCapturado.getNombre());
+        assertEquals(latitud, puntoCapturado.getLatitud(), 0.0);
+        assertEquals(longitud, puntoCapturado.getLongitud(), 0.0);
         verify(vistaMock).mostrarMensaje("Punto de interés guardado");
 
-        // Caso no valido (Ya existe el punto de interes)
-        presenter.onGuardarPuntoInteresClicked(nombreStr, latitudStr, longitudStr);
+        // Caso no valido (Ya existe el punto de interes con ese nombre)
+        presenter.onGuardarPuntoInteresClicked(nombreStr, "43.4733", "-3.80111");
         verify(vistaMock).mostrarMensaje("Ya existe un punto de interés con ese nombre");
 
-        // Caso no valido (Error acceso a BBDD)
+        /* Caso no valido (Error acceso a BBDD)
+        presenter.onGuardarPuntoInteresClicked("pabellon", "43.47578", "-3.76644");
+        verify(vistaMock).mostrarMensaje("Ha ocurrido un error en la base de datos");*/
 
     }
 }
