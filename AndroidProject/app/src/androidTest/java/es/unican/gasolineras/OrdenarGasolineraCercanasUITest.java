7269777d0de93a56fc50ed1628be40ceaa542a5c
scg194 <scg194@alumnos.unican.es>
2024-10-23 16:40:24 +0200

    finalizado test Espresso OrdenarCercania

--- AndroidProject/app/src/androidTest/java/es/unican/gasolineras/OrdenarGasolineraCercanasUITest.java
+++ AndroidProject/app/src/androidTest/java/es/unican/gasolineras/OrdenarGasolineraCercanasUITest.java
@@ -3,19 +3,29 @@ package es.unican.gasolineras;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.anything;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
-import static java.util.EnumSet.allOf;
-
+import static org.junit.Assert.assertEquals;
 import static es.unican.gasolineras.utils.MockRepositories.getTestRepository;
 
 import android.content.Context;
+import android.widget.ListView;
 
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -25,6 +35,7 @@ import dagger.hilt.android.testing.HiltAndroidTest;
 import dagger.hilt.android.testing.UninstallModules;
 import es.unican.gasolineras.activities.main.MainView;
 import es.unican.gasolineras.injection.RepositoriesModule;
+import es.unican.gasolineras.model.Gasolinera;
 import es.unican.gasolineras.model.PuntoInteres;
 import es.unican.gasolineras.repository.IGasolinerasRepository;
 
@@ -46,16 +57,52 @@ public class OrdenarGasolineraCercanasUITest {
     final IGasolinerasRepository repository = getTestRepository(context, R.raw.gasolineras_ccaa_06);
 
 
+    @Before
+    public void inicializa(){
+        //estado inicial base de datos Ptos Interes vacia
+
+    }
+
     @Test
     public void testOrdenaGasolinerasCercanas() {
 
+        //creo un punto de interes
+        openActionBarOverflowOrOptionsMenu(context);
+        onView((withText("Añadir Punto interés"))).perform(click());
+        onView(withId(R.id.etNombre)).perform(click());
+        onView(withId(R.id.etNombre)).perform(typeText("casa"));
+        Espresso.closeSoftKeyboard();
+
+        onView(withId(R.id.etLatitud)).perform(click());
+        onView(withId(R.id.etLatitud)).perform(typeText("43.3089400"));
+        Espresso.closeSoftKeyboard();
+
+        onView(withId(R.id.etLongitud)).perform(click());
+        onView(withId(R.id.etLongitud)).perform(typeText("-4.2301600"));
+        Espresso.closeSoftKeyboard();
+
+        onView(withId(R.id.buttonGuardar)).perform(click());
+
+        //clicka en filtrar
         onView(withId(R.id.menuFiltrar)).perform(click());
 
+        //clicka en el selector de Pto Interes
         onView(withId(R.id.spinnerPtosInteres)).perform(click());
 
-        onData(allOf(is(instanceOf(String.class)), is("casa")));
+        //elige la opcion casa
+        onData(allOf(is(instanceOf(String.class)),
+                is("casa"))).inRoot(isPlatformPopup()).perform(click());
 
+        //clicka el boton ordenar
         onView(withId(R.id.btnOrdenar)).perform(click());
+
+        //comprueba la direccion de la primera gasolinera
+        DataInteraction g1 = onData(anything()).inAdapterView(withId(R.id.lvStations)).atPosition(0);
+        g1.onChildView(withId(R.id.tvAddress)).check(matches(withText("CALLE CORREOS, SN")));
+
+        //comprueba la direccion de la segunda gasolinera
+        DataInteraction g2 = onData(anything()).inAdapterView(withId(R.id.lvStations)).atPosition(0);
+        g2.onChildView(withId(R.id.tvAddress)).check(matches(withText("CARRETERA 634 KM. 244")));
     }
 }
 
